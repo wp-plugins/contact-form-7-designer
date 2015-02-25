1daab1b2181b956f558fd6131b7f55875c85f754
tinygiantstudios <tinygiantstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 20:50:15 +0000

    Initial Upload
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-designer/tags/1.0@1099575 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cf7-styles.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: Contact Form 7 Designer
+Version: 1.0
+Plugin URI: http://tinygiantstudios.co.uk
+Author: Tiny Giant Studios
+Author URI: http://www.tinygiantstudios.co.uk
+Description: Contact Form 7 Designer is an add-on for Contact Form 7 that allows you to add custom designs for your contact form, without requiring any coding knowledge.
+*/
+
+
+
+// Using the TGM activation class, install and activate the Redux Framework plugin
+include_once ('class-tgm-plugin-activation.php');
+
+function cf7_styles_register_required_plugins() {
+	$plugins = array(
+		array(
+			'name' 		=> 'Redux Framework',
+			'slug' 		=> 'redux-framework',
+			'required' 	=> true,
+			'force_activation'   => true,
+		),
+	);
+/*
+	$config = array(
+		'domain'       		=> 'redux-framework',         	// Text domain - likely want to be the same as your theme.
+		'default_path' 		=> '',                         	// Default absolute path to pre-packaged plugins
+		'parent_menu_slug' 	=> 'plugins.php', 				// Default parent menu slug
+		'parent_url_slug' 	=> 'plugins.php', 				// Default parent URL slug
+		'menu'         		=> 'install-required-plugins', 	// Menu slug
+		'has_notices'      	=> true,                       	// Show admin notices or not
+		'is_automatic'    	=> true,					   	// Automatically activate plugins after installation or not
+	);
+*/
+	 $config = array(
+        'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => true,                    // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
+        'strings'      => array(
+            'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
+            'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+            'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+            'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+            'nag_type'                        => 'error' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+        )
+    );
+
+	tgmpa( $plugins, $config );
+}
+add_action( 'tgmpa_register', 'cf7_styles_register_required_plugins' );
+
+
+
+
+// Load the Redux Framework Class from the Redux Framework Plugin
+$cf7_redux_dir = plugin_dir_path( __DIR__ );
+if (!class_exists( 'ReduxFramework' ) && file_exists( $cf7_redux_dir . 'redux-framework/ReduxCore/framework.php')) {
+    require_once( $cf7_redux_dir . 'redux-framework/ReduxCore/framework.php' );
+} 
+
+
+
+// Remove the demo links from Redux Framework Plugin
+function cf7_styles_remove_redux_links() { // Be sure to rename this function to something more unique
+    if ( class_exists('ReduxFrameworkPlugin') ) {
+        remove_filter( 'plugin_row_meta', array( ReduxFrameworkPlugin::get_instance(), 'plugin_metalinks'), null, 2 );
+    }
+    if ( class_exists('ReduxFrameworkPlugin') ) {
+        remove_action('admin_notices', array( ReduxFrameworkPlugin::get_instance(), 'admin_notices' ) );    
+    }
+}
+add_action('init', 'cf7_styles_remove_redux_links');
+
+
+
+// Load the custom settings panel to go with Redux Framework plugin
+require_once( dirname( __FILE__ ) . '/cf7-styles-config.php' );
+
+
+
+// Load custom plugin scripts for the front end
+function cf7_styles_enqueue() {
+	wp_enqueue_script( 'jquery' );
+
+	wp_register_script('cf7_styles_scripts', plugins_url('/includes/js/cf7-styles.js', __FILE__), array('jquery'));
+	wp_enqueue_script( 'cf7_styles_scripts' );
+}
+
+add_action( 'wp_enqueue_scripts', 'cf7_styles_enqueue' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,121 @@
+=== Contact Form 7 Designer ===
+Contributors: tinygiantstudios
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=info@tinygiantstudios.co.uk&item_name=Donation+for+Contact+Form+7+Styles
+Tags: contact form 7, CF7, contact form, CF, contact form 7 css, CF7 css, contact form 7 styles, CF styles, contact form 7 designer, cf7 designer, forms, form css, form styling, form stylesheet, form css stylesheet, quform, gravity forms, ninja forms
+Requires at least: 3.8
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Contact Form 7 Designer allows users to design their form to fit in with the look & feel of their website.
+
+== Description ==
+
+= Contact Form 7 Designer =
+
+*Making your contact forms look nice should be easy. Finally it is!*
+
+Contact Form 7 Designer lets you design a highly customisable contact form which you can display wherever you want it using a shortcode. All designs are easy to create with a comprehensive backend editor. The best thing is: You don't need any coding skills whatsoever.
+
+**Contact Form 7 Designer, at a glance..**
+
+- Simple. All you need is WordPress and the base Contact Form 7 plugin.
+- Beautiful. Your form designs are limited only by your creativity.
+- Customizable. With the Pro version, you can customize even more fields.
+- Integrates with Contact Form 7 and all the other add-ons.
+- Developer friendly.
+
+> #### Contact Form 7 Designer Pro
+> This plugin has a premium version which comes with the following features.<br />
+>
+> Ability to add designs for _ALL_ fields<br />
+> No advertising or premium upgrade notices<br />
+> Easy import / export functionality to transfer designs<br />
+> Use across multiple websites without extra licenses<br />
+> Access to sample forms that you can import<br />
+> WordPress customizer integration for real-time design (coming soon)<br />
+> Priority Support<br />
+>
+> [Upgrade to Contact Form 7 Designer Pro](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=after-features-link)
+
+**Add-on plugins**
+
+There are several add-on plugins available for Contact Form 7, which help you get even more out of your site.
+
+- [Gravity Form Importer for Contact Form 7](https://wordpress.org/plugins/contact-form-7-gravity-forms/) - Imports forms from the Gravity Forms plugin.
+- [Contact Form 7 Honeypot](https://wordpress.org/plugins/contact-form-7-honeypot/) - Adds enhanced anti-spam features to your contact form.
+
+**Bug Reports**
+
+Bug reports for [Contact Form 7 Designer are welcomed on GitHub](https://github.com/TinyGiantStudios/cf7-styles). Please note that GitHub is _not_ a support forum.
+
+**More information**
+
+- [Contact Form 7 Designer Pro](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/), containing [additional features](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/).
+- Developers: fFllow or contribute to the [Contact Form 7 Designer on GitHub](https://github.com/TinyGiantStudios/cf7-styles)
+- Other [WordPress plugins](http://tinygiantstudios.co.uk/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=more-info-link) by [Tiny Giant Studios](http://tinygiantstudios.co.za/)
+- [@tinygiantstudio](https://twitter.com/tinygiantstudio) on Twitter
+
+
+== Installation ==
+
+= Installing the plugin =
+1. In your WordPress admin panel, go to *Plugins > New Plugin*, search for **Contact Form 7 Designer** and click "*Install now*"
+2. Alternatively, download the plugin and upload the contents of `cf7-styles.zip` to your plugins directory, which usually is `/wp-content/plugins/`.
+3. Activate the plugin
+4. Navigate to Form Styles and start adding your designs.
+
+Need help? Please take a look at the FAQ section first. If you can't find your answer there, start a topic in the Support tab.
+
+= Upgrade to Contact Form 7 Designer Pro =
+If you like the plugin, [get the Pro version of Contact Form 7 Designer](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=installation-instructions-link) for an even better plugin!
+
+== Frequently Asked Questions ==
+
+> **What are the main differences between Pro and Lite?**<br />
+>
+> Contact Form 7 Designer Pro comes with some additional features.<br />
+>
+> **All Fields.** can be styled, instead of just the input, textarea and button fields.<br />
+> **Import / Export.** Adds the ability to transfer your designs across multiple sites.<br />
+> **WP Customizer.** Lets you design your form in real time (Coming Soon).<br />
+> **Premium Support** Allows you to log a support ticket for premium support.<br /><br />
+>
+> [Differences Lite & Pro](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=faq-link) | [Demo](http://contactform7.tgsdemos.com/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=faq-link) | [Upgrade Now](http://tinygiantstudios.co.uk/?add-to-cart=6921/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=faq-link)
+
+= How to display a form in posts or pages? =
+Use the shortcode provided by Contact Form 7.
+
+= How can I style the sign-up form? =
+Simply use this plugin to your heart's content. If you need the more advanced fields, you should consider the [Pro version](http://tinygiantstudios.co.uk/product/contact-form-7-designer-pro/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=faq-link).
+
+[>> With the Pro plugin it's really easy to design beautiful forms <<](https://mc4wp.com/#utm_source=wp-plugin-repo&utm_medium=link&utm_campaign=after-css-link)
+
+== Other Notes ==
+
+= Bug Reports =
+
+Bug reports for [Contact Form 7 Designer are welcomed on GitHub](https://github.com/TinyGiantStudios/cf7-styles). Please note that GitHub is _not_ a support forum.
+
+== Screenshots ==
+
+1. Simple or advanced MailChimp sign-up forms that blend in with your theme.
+2. A sign-up checkbox in your comment form is an amazing conversion booster.
+3. A simple form in the footer of the Twenty Thirteen theme.
+4. Add sign-up checkboxes to various places on your site.
+5. Creating sign-up forms for your MailChimp lists is easy. The Pro version allows you to create as many form as you like.
+6. Write your own HTML or have it generated for you. Many (optional) customization settings are availabl.
+7. **Pro only:** Gain valuable insights which method your visitors used to subscribe for any given time period using beautiful line charts.
+8. **Pro only:** Create your own CSS styles with the form designer in the Pro version.
+
+== Changelog ==
+
+= 1.0 - 17 February 2015 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+The start of great things to come.
